a8a9e700b3e8af8f2ef2630fa445ac859e650b65
jylee2 <leej1025y@live.com>
2020-11-24 11:32:28 +0800

    Separated html and js

---
+++ input.js
@@ -0,0 +1,25 @@
+//Define the default input state
+const defaultInput = `# Heading Level 1
+## Heading Level 2
+### Heading Level 3
+
+Link: [OpenFOAM](https://www.openfoam.com/)
+
+\`inline code\`
+
+\`\`\`
+//code block
+\`\`\`
+
+- List
+  - item
+
+> Block Quote
+
+![OpenFOAM Logo](https://pbs.twimg.com/profile_images/1171023226/image_400x400.png)
+
+**bold text**
+
+text text
+
+`;
