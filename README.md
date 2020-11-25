8976950b3a5e33fc38c2863232c8cd81617c9f28
jylee2 <leej1025y@live.com>
2020-11-25 11:52:30 +0800

    Add a drum machine

--- README.md
+++
@@ -1,1 +0,0 @@
-### freeCodeCamp Front End Libraries Projects: [Build a Markdown Previewer](https://jylee2.github.io/markdown-previewer/)
---
+++ main.js
@@ -0,0 +1,50 @@
+// Initialise inputKey var
+let inputKey;
+// Initialise keyDisp var
+let keyDisp = "";
+// playAudio functions linked to each button, it plays the audio file based on the button clicked
+const playAudioQ = () => {
+  keyDisp = "Q";
+  inputKey = document.getElementById(keyDisp);
+  return inputKey.play();
+}
+const playAudioW = () => {
+  keyDisp = "W";
+  inputKey = document.getElementById(keyDisp);
+  return inputKey.play();
+}
+const playAudioE = () => {
+  keyDisp = "E";
+  inputKey = document.getElementById(keyDisp);
+  return inputKey.play();
+}
+const playAudioA = () => {
+  keyDisp = "A";
+  inputKey = document.getElementById(keyDisp);
+  return inputKey.play();
+}
+const playAudioS = () => {
+  keyDisp = "S";
+  inputKey = document.getElementById(keyDisp);
+  return inputKey.play();
+}
+const playAudioD = () => {
+  keyDisp = "D";
+  inputKey = document.getElementById(keyDisp);
+  return inputKey.play();
+}
+const playAudioZ = () => {
+  keyDisp = "Z";
+  inputKey = document.getElementById(keyDisp);
+  return inputKey.play();
+}
+const playAudioX = () => {
+  keyDisp = "X";
+  inputKey = document.getElementById(keyDisp);
+  return inputKey.play();
+}
+const playAudioC = () => {
+  keyDisp = "C";
+  inputKey = document.getElementById(keyDisp);
+  return inputKey.play();
+}
